15ab2c8a04806bb0a4be2e0c588ac5346d742180
foolingdutchman <365300312@qq.com>
2018-07-29 23:33:02 +0800

    submit email alert

--- web/public/js/mail-script.js
+++ web/public/js/mail-script.js
@@ -15,13 +15,16 @@
                 dataType: 'html', // request type html/json/xml
                 data: form.serialize(), // serialize form data
                 beforeSend: function() {
-                    alert.fadeOut();
-                    submit.html('Sending....'); // change submit button text
+                    alertMsg("发送成功！")
+                    // alert.fadeOut();
+                    // submit.html('Sending....'); // change submit button text
+                   
                 },
                 success: function(data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
+                    savedata(data)
+                    // alert.html(data).fadeIn(); // fade in response data
+                    // form.trigger('reset'); // reset form
+                    // submit.attr("style", "display: none !important");; // reset submit button text
                 },
                 error: function(e) {
                     console.log(e)
@@ -29,3 +32,34 @@
             });
         });
     });
+    function alertMsg(text) {
+        var bgHtml ="<div class='msg-bg'></div>";
+        var msgAlertHtml = "<div class='msg-alertWrap'>" +
+                                "<div class='msg-alert'>" +
+                                    "<div class='msg-header'><button class='msg-dismiss'><span>&times;</span></button><h4 class='msg-title'>温馨提示</h4></div>" +
+                                    "<div class='msg-body'>" +
+                                        "<div class='msg-content'></div>" +
+                                    "</div>" +
+                                    "<div class='msg-footer'>" +
+                                        "<button type='button' class='msg-btnOk'>确定</button>" +
+                                    "</div>" +
+                                "</div>"+
+                            "</div>";
+        if ($(".msg-bg").length == 0) {
+            $("body").append(bgHtml + msgAlertHtml);
+        } else {
+            $("body").append(msgAlertHtml);
+        }
+    
+        $(".msg-alertWrap:last-child .msg-content").html(text);
+    
+        $(".msg-btnOk,.msg-dismiss").on("click", function () {
+            $(this).parents(".msg-alertWrap").remove();
+            if ($(".msg-alertWrap").length == 0) {
+                $(".msg-bg").remove();
+            }
+        })
+    }
+    function savedata(data){
+        alertMsg(data.fname+","+data.email+","+data.message) 
+    }
